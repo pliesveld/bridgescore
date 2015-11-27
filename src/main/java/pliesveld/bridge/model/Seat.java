fc7bf9a35effb6b7520fe4060777a15b74f2149c
Patrick Liesveld <pliesveld@users.noreply.github.com>
2015-11-27 02:19:34 -0500

    switched from combobox to radio choice for form components

--- src/main/java/pliesveld/bridge/model/Seat.java
+++ src/main/java/pliesveld/bridge/model/Seat.java
@@ -8,30 +8,32 @@ public enum Seat
     SOUTH {
         Team team()
         { return Team.TEAM_NS; }
-	Seat next()
-	{ return Seat.WEST; }
-    },
-    EAST {
-        Team team()
-        { return Team.TEAM_WE; }
-	Seat next()
-	{ return Seat.SOUTH; }
-    },
-    NORTH {
-        Team team()
-        { return Team.TEAM_NS; }
-	Seat next()
-	{ return Seat.EAST; }
+
+        Seat next()
+        { return Seat.WEST; }
     },
     WEST {
         Team team()
         { return Team.TEAM_WE; }
-	Seat next()
-	{ return Seat.NORTH; }
+
+        Seat next()
+        { return Seat.NORTH; }
+    },
+    NORTH {
+        Team team()
+        { return Team.TEAM_NS; }
+
+        Seat next()
+        { return Seat.EAST; }
+    },
+    EAST {
+        Team team()
+        { return Team.TEAM_WE; }
+
+        Seat next()
+        { return Seat.SOUTH; }
     };
 
     abstract Team team();
     abstract Seat next();
-
-
 }
